7b041811eaf5c251223aac985e9ba5d4c30ac2d1
levakuz <levakuz007@gmail.com>
2021-03-01 15:41:31 +0300

    first commit

---
+++ src/environment_listener.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+import rospy
+import json
+from geometry_msgs.msg import Quaternion, Pose
+from std_msgs.msg import Float64
+
+import pika
+
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.33',
+                                                               5672,
+                                                               '/',
+                                                               credentials))
+
+channel = connection.channel()
+
+channel.exchange_declare("velocity", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+channel.exchange_declare("direction", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+channel.exchange_declare("temperature", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+channel.exchange_declare("pressure", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+
+
+def wind_velocity(data):
+    """Option 1"""
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    vel_data = {"velocity": data.data}
+    print(vel_data)
+    channel.basic_publish(
+        exchange='velocity',
+        routing_key="wind",
+        body=json.dumps(vel_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def wind_direction_callback(data):
+    """ Option 2"""
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    vel_data = {"direction":{}}
+    vel_data["velocity"] = data.velocity
+    vel_data["direction"]["x"] = data.direction.x
+    vel_data["direction"]["y"] = data.direction.y
+    vel_data["direction"]["z"] = data.direction.z
+    channel.basic_publish(
+        exchange='direction',
+        routing_key="wind",
+        body=json.dumps(vel_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def temperature_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    temp_data = {"temperature": data.data}
+    channel.basic_publish(
+        exchange='temperature',
+        routing_key="",
+        body=json.dumps(temp_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def pressure_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    pres_data = {"pressure": data.data}
+    print(pres_data)
+    channel.basic_publish(
+        exchange='pressure',
+        routing_key="",
+        body=json.dumps(pres_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def listener():
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('environment_interface_listener', anonymous=True)
+    rospy.Subscriber("/wind/velocity", Float64, wind_velocity)
+    rospy.Subscriber("/wind/diection", Pose, wind_direction_callback)
+    rospy.Subscriber("/temperature/temp_var", Float64, temperature_callback)
+    rospy.Subscriber("/pressure/pressure_var", Float64, pressure_callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
---
+++ src/uav_listener.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import rospy
+import json
+from geometry_msgs.msg import Quaternion, Pose
+from sensor_msgs.msg import BatteryState
+from mavros_msgs.msg import Altitude
+
+import pika
+
+
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.33',
+                                                               5672,
+                                                               '/',
+                                                               credentials))
+
+channel = connection.channel()
+print(channel)
+
+channel.exchange_declare("geoposition", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+channel.exchange_declare("battery", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+channel.exchange_declare("altitude", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+
+
+def local_position_uav_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    json_data = {"coordinates": {}, "angles": {}}
+    json_data["coordinates"]["x"] = data.position.x
+    json_data["coordinates"]["y"] = data.position.y
+    json_data["coordinates"]["z"] = data.position.z
+    json_data["angles"]["x"] = data.orientation.x
+    json_data["angles"]["y"] = data.orientation.y
+    json_data["angles"]["z"] = data.orientation.z
+    json_data["angles"]["w"] = data.orientation.w
+    channel.basic_publish(
+        exchange='geoposition',
+        routing_key="UAV_local",
+        body=json.dumps(json_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def global_position_uav_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+    json_data = {"coordinates": {}, "angles": {}}
+    json_data["coordinates"]["x"] = data.position.x
+    json_data["coordinates"]["y"] = data.position.y
+    json_data["coordinates"]["z"] = data.position.z
+    json_data["angles"]["x"] = data.orientation.x
+    json_data["angles"]["y"] = data.orientation.y
+    json_data["angles"]["z"] = data.orientation.z
+    json_data["angles"]["w"] = data.orientation.w
+    channel.basic_publish(
+        exchange='geoposition',
+        routing_key="UAV_local",
+        body=json.dumps(json_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def global_position_uav_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    json_data = {"coordinates": {}, "angles": {}}
+    json_data["coordinates"]["x"] = data.position.x
+    json_data["coordinates"]["y"] = data.position.y
+    json_data["coordinates"]["z"] = data.position.z
+    json_data["angles"]["x"] = data.orientation.x
+    json_data["angles"]["y"] = data.orientation.y
+    json_data["angles"]["z"] = data.orientation.z
+    json_data["angles"]["w"] = data.orientation.w
+    channel.basic_publish(
+        exchange='geoposition',
+        routing_key="UAV_global",
+        body=json.dumps(json_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def voltage_uav_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.voltage)
+    json_data = {"charge": data.voltage}
+    channel.basic_publish(
+        exchange='battery',
+        routing_key="UAV_global",
+        body=json.dumps(json_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def altitude_uav_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+    json_data = {"altitude": data.terrain}
+    print(json_data)
+    channel.basic_publish(
+        exchange='altitude',
+        routing_key="UAV_global",
+        body=json.dumps(json_data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+
+def listener():
+    rospy.init_node('uav_interface_listener', anonymous=True)
+    rospy.Subscriber("/mavros/local_position", Pose, local_position_uav_callback)
+    rospy.Subscriber("/mavros/global_position", Pose, global_position_uav_callback)
+    rospy.Subscriber("/mavros/battery", BatteryState, voltage_uav_callback)
+    rospy.Subscriber("/mavros/altitude", Altitude, altitude_uav_callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
