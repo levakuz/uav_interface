14f9c3abd1f3f9f8963b81d8d33a0637261e253f
levakuz <levakuz007@gmail.com>
2021-04-19 18:27:25 +0300

    fixes DB

--- src/SenderToROS.py
+++ src/SenderToROS.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 import rospy
-import  json
+import json
 from std_msgs.msg import String
-from geometry_msgs.msg import Quaternion, Pose
+from geometry_msgs.msg import Quaternion, PoseStamped
 from std_msgs.msg import Float64
+from std_msgs.msg import Header
 import psycopg2
 from psycopg2 import Error
 import pika
 import datetime
 
 
+
 credentials = pika.PlainCredentials('admin', 'admin')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.33',
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.71',
                                                                5672,
                                                                '/',
                                                                credentials))
@@ -32,33 +34,29 @@ channel.exchange_declare("pressure", exchange_type='topic', passive=False,
                          durable=False, auto_delete=False, arguments=None)
 channel.exchange_declare("battery", exchange_type='topic', passive=False,
                          durable=False, auto_delete=False, arguments=None)
-
-
-channel.queue_bind(exchange='goals', queue='CO_goals', routing_key='CO')
+channel.exchange_declare("environment", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
+channel.exchange_declare("altitude", exchange_type='topic', passive=False,
+                         durable=False, auto_delete=False, arguments=None)
 
 channel.queue_declare(queue='CO_goals', durable=False)
-
-
-channel.queue_bind(exchange='environment', queue='wind_direction', routing_key='wind_direction')
-channel.queue_bind(exchange='environment', queue='wind_velocity', routing_key='wind_velocity')
-channel.queue_bind(exchange='environment', queue='temperature', routing_key='temperature')
-channel.queue_bind(exchange='environment', queue='pressure', routing_key='pressure')
-
-
 channel.queue_declare(queue='wind_direction', durable=False)
 channel.queue_declare(queue='wind_velocity', durable=False)
 channel.queue_declare(queue='temperature', durable=False)
 channel.queue_declare(queue='pressure', durable=False)
+channel.queue_declare(queue='UAV_altitude', durable=False)
+channel.queue_declare(queue='UAV_local_position', durable=False)
 
-
+channel.queue_bind(exchange='goals', queue='CO_goals', routing_key='CO')
+channel.queue_bind(exchange='goals', queue='UAV_local_position', routing_key='UAV')
+channel.queue_bind(exchange='environment', queue='wind_direction', routing_key='wind_direction')
+channel.queue_bind(exchange='environment', queue='wind_velocity', routing_key='wind_velocity')
+channel.queue_bind(exchange='environment', queue='temperature', routing_key='temperature')
+channel.queue_bind(exchange='environment', queue='pressure', routing_key='pressure')
 channel.queue_bind(exchange='altitude', queue='UAV_altitude', routing_key='UAV_altitude')
 
 
-channel.queue_declare(queue='UAV_altitude', durable=False)
-
-
 connection_db = psycopg2.connect(user="postgres",
-                              # пароль, который указали при установке PostgreSQL
                               password="vfvfcdtnf",
                               host="127.0.0.1",
                               port="5432",
@@ -66,12 +64,11 @@ connection_db = psycopg2.connect(user="postgres",
 
 
 
-
 def goal_co_callback(ch, method, properties, body):
     time = datetime.datetime.now().time()
-    pub = rospy.Publisher('/sim_target/waypoint', Pose, queue_size=10)
-    rospy.init_node('goal_co_interface_pub', anonymous=True)
-    data = Pose()
+    pub = rospy.Publisher('/sim_target/waypoint', PoseStamped, queue_size=10)
+    # rospy.init_node('goal_co_interface_pub', anonymous=True)
+    data = PoseStamped()
     data.position.x = body["coordinates"]["x"]
     data.position.y = body["coordinates"]["y"]
     data.position.z = body["coordinates"]["z"]
@@ -94,7 +91,7 @@ def wind_velocity(ch, method, properties, body):
     """Option 1"""
     time = datetime.datetime.now().time()
     pub = rospy.Publisher('/wind/velocity', Float64, queue_size=10)
-    rospy.init_node('wind_velocity_interface_pub', anonymous=True)
+    # rospy.init_node('wind_velocity_interface_pub', anonymous=True)
     data = Float64()
     data.data = body["velocity"]
     pub.publish(data)
@@ -111,9 +108,9 @@ def wind_velocity(ch, method, properties, body):
 def wind_direction_callback(ch, method, properties, body):
     """ Option 2"""
     time = datetime.datetime.now().time()
-    pub = rospy.Publisher('/wind/direction', Pose, queue_size=10)
-    rospy.init_node('wind_direction_interface_pub', anonymous=True)
-    data = Pose()
+    pub = rospy.Publisher('/wind/direction', PoseStamped, queue_size=10)
+    # rospy.init_node('wind_direction_interface_pub', anonymous=True)
+    data = PoseStamped()
     data.velocity = body["velocity"]
     data.direction.x = body["direction"]["x"]
     data.direction.y = body["direction"]["y"]
@@ -133,7 +130,7 @@ def wind_direction_callback(ch, method, properties, body):
 def temperature_callback(ch, method, properties, body):
     time = datetime.datetime.now().time()
     pub = rospy.Publisher('/temperature/temp_var', Float64, queue_size=10)
-    rospy.init_node('temperature_interface_pub', anonymous=True)
+    # rospy.init_node('temperature_interface_pub', anonymous=True)
     data = Float64()
     data.data = body["temperature"]
     pub.publish(data)
@@ -150,10 +147,37 @@ def temperature_callback(ch, method, properties, body):
 
 def altitude_uav_callback(ch, method, properties, body):
     pub = rospy.Publisher('/mavros/altitude', Float64, queue_size=10)
-    rospy.init_node('altitude_interface_pub', anonymous=True)
+    # rospy.init_node('altitude_interface_pub', anonymous=True)
     data = Float64()
     data.data = body["altitude"]
     pub.publish(data)
+
+
+def uav_local_position_callback(ch, method, properties, body):
+    rospy.init_node('uav_local_position', anonymous=True)
+    print(body)
+    body = json.loads(body)
+    rate = rospy.Rate(20)
+    print(body["coordinates"]["x"])
+    pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=20)
+
+    data = PoseStamped()
+    # data.header = Header()
+    data.header.stamp = rospy.Time.now()
+    data.header.frame_id = "map"
+    data.pose.position.x = int(body["coordinates"]["x"])
+    data.pose.position.y = int(body["coordinates"]["y"])
+    data.pose.position.z = int(body["coordinates"]["z"])
+    # data.pose.orientation.x = float(body["angles"]["x"])
+    # data.pose.orientation.y = float(body["angles"]["y"])
+    # data.pose.orientation.z = float(body["angles"]["z"])
+    # data.pose.orientation.w = float(body["angles"]["w"])
+    pub.publish(data)
+    print(data)
+    while not rospy.is_shutdown():
+        pub.publish(data)
+        rate.sleep()
+        print(data)
 
 
 channel.basic_consume(queue='CO_goals', on_message_callback=goal_co_callback, auto_ack=True)
@@ -161,9 +185,10 @@ channel.basic_consume(queue='wind_direction', on_message_callback=wind_direction
 channel.basic_consume(queue='wind_velocity', on_message_callback=wind_velocity, auto_ack=True)
 channel.basic_consume(queue='temperature', on_message_callback=temperature_callback, auto_ack=True)
 channel.basic_consume(queue='UAV_altitude', on_message_callback=altitude_uav_callback, auto_ack=True)
+channel.basic_consume(queue='UAV_local_position', on_message_callback=uav_local_position_callback, auto_ack=True)
 
 
 if __name__ == '__main__':
     print(' [*] Waiting for messages. To exit press CTRL+C')
-    rospy.spin()
+    # rospy.init_node('interface_publisher', anonymous=True)
     channel.start_consuming()
--- src/uav_listener.py
+++ src/uav_listener.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import time
+
 import rospy
 import json
-from geometry_msgs.msg import Quaternion, Pose
+from geometry_msgs.msg import Quaternion, PoseStamped
 from sensor_msgs.msg import BatteryState
 from mavros_msgs.msg import Altitude
 
@@ -11,10 +13,10 @@ from psycopg2 import Error
 import datetime
 
 credentials = pika.PlainCredentials('admin', 'admin')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.33',
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.71',
                                                                5672,
                                                                '/',
-                                                               credentials))
+                                                               credentials,blocked_connection_timeout=0,heartbeat=0))
 
 channel = connection.channel()
 print(channel)
@@ -27,98 +29,202 @@ channel.exchange_declare("altitude", exchange_type='topic', passive=False,
                          durable=False, auto_delete=False, arguments=None)
 
 connection_db = psycopg2.connect(user="postgres",
-                              password="1111",
+                              password="vfvfcdtnf",
                               host="127.0.0.1",
                               port="5432",
                               database="postgres_db")
 
+old_local_data = {}
+old_global_data = {}
+old_voltage_data = 0
+altitude_old_data = 0
+
 
 def local_position_uav_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    time = datetime.datetime.now().time().strftime("%H:%M:%S")
+    # print(time)
+    global old_local_data
+    # rospy.loginfo(data)
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
     json_data = {"coordinates": {}, "angles": {}}
-    json_data["coordinates"]["x"] = data.position.x
-    json_data["coordinates"]["y"] = data.position.y
-    json_data["coordinates"]["z"] = data.position.z
-    json_data["angles"]["x"] = data.orientation.x
-    json_data["angles"]["y"] = data.orientation.y
-    json_data["angles"]["z"] = data.orientation.z
-    json_data["angles"]["w"] = data.orientation.w
-    channel.basic_publish(
-        exchange='geoposition',
-        routing_key="UAV_local",
-        body=json.dumps(json_data),
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-        ))
+    json_data["coordinates"]["x"] = '{:.2f}'.format(data.pose.position.x)
+    json_data["coordinates"]["y"] = '{:.2f}'.format(data.pose.position.y)
+    json_data["coordinates"]["z"] = '{:.2f}'.format(data.pose.position.z)
+    json_data["angles"]["x"] = '{:.2f}'.format(data.pose.orientation.x)
+    json_data["angles"]["y"] = '{:.2f}'.format(data.pose.orientation.y)
+    json_data["angles"]["z"] = '{:.2f}'.format(data.pose.orientation.z)
+    json_data["angles"]["w"] = '{:.2f}'.format(data.pose.orientation.w)
+    # print(json_data)
+
+    if old_local_data != json_data:
+        # print("here")
+        channel.basic_publish(
+                exchange='geoposition',
+                routing_key="UAV_local",
+                body=json.dumps(json_data),
+                properties=pika.BasicProperties(
+                    delivery_mode=2,
+                ))
+        cursor = connection_db.cursor()
+        insert_query = """ UPDATE uav_dynamic_params SET coords = '{}' WHERE time = '{}' AND id = {};
+        """.format(json.dumps(json_data), time, 1)
+        cursor.execute(insert_query)
+        connection_db.commit()
+        count = cursor.rowcount
+        print(count, "Succesfull update")
+        if count == 0:
+            cursor = connection_db.cursor()
+            insert_query = """ INSERT INTO uav_dynamic_params (time, id, coords)
+                                                       VALUES (%s, %s, %s)"""
+            item_tuple = (time, 1, json.dumps(json_data))
+            cursor.execute(insert_query, item_tuple)
+            connection_db.commit()
+            count = cursor.rowcount
+            print(count, "Succesfull INSERT")
+        rospy.sleep(5)
+    old_local_data = json_data
+
+
 
 
 def global_position_uav_callback(data):
+    time = datetime.datetime.now().time().strftime("%H:%M:%S")
+    global old_global_data
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
     json_data = {"coordinates": {}, "angles": {}}
-    json_data["coordinates"]["x"] = data.position.x
-    json_data["coordinates"]["y"] = data.position.y
-    json_data["coordinates"]["z"] = data.position.z
-    json_data["angles"]["x"] = data.orientation.x
-    json_data["angles"]["y"] = data.orientation.y
-    json_data["angles"]["z"] = data.orientation.z
-    json_data["angles"]["w"] = data.orientation.w
-    channel.basic_publish(
-        exchange='geoposition',
-        routing_key="UAV_local",
-        body=json.dumps(json_data),
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-        ))
+    json_data["coordinates"]["x"] = '{:.2f}'.format(data.pose.position.x)
+    json_data["coordinates"]["y"] = '{:.2f}'.format(data.pose.position.y)
+    json_data["coordinates"]["z"] = '{:.2f}'.format(data.pose.position.z)
+    json_data["angles"]["x"] = '{:.2f}'.format(data.pose.orientation.x)
+    json_data["angles"]["y"] = '{:.2f}'.format(data.pose.orientation.y)
+    json_data["angles"]["z"] = '{:.2f}'.format(data.pose.orientation.z)
+    json_data["angles"]["w"] = '{:.2f}'.format(data.pose.orientation.w)
+    if old_global_data != json_data:
+        channel.basic_publish(
+            exchange='geoposition',
+            routing_key="UAV_local",
+            body=json.dumps(json_data),
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+            ))
+        cursor = connection_db.cursor()
+        insert_query = """ UPDATE uav_dynamic_params SET coords = '{}' WHERE time = '{}' AND id = {};
+                """.format(json.dumps(json_data), time, 1)
+        cursor.execute(insert_query)
+        connection_db.commit()
+        count = cursor.rowcount
+        print(count, "Succesfull update")
+        if count == 0:
+            cursor = connection_db.cursor()
+            insert_query = """ INSERT INTO uav_dynamic_params (time, id, coords)
+                                                       VALUES (%s, %s, %s)"""
+            item_tuple = (time, 1, json.dumps(json_data))
+            cursor.execute(insert_query, item_tuple)
+            connection_db.commit()
+            count = cursor.rowcount
+            print(count, "Succesfull update")
+    old_global_data = json_data
+    rospy.sleep(5)
 
 
-def global_position_uav_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    json_data = {"coordinates": {}, "angles": {}}
-    json_data["coordinates"]["x"] = data.position.x
-    json_data["coordinates"]["y"] = data.position.y
-    json_data["coordinates"]["z"] = data.position.z
-    json_data["angles"]["x"] = data.orientation.x
-    json_data["angles"]["y"] = data.orientation.y
-    json_data["angles"]["z"] = data.orientation.z
-    json_data["angles"]["w"] = data.orientation.w
-    channel.basic_publish(
-        exchange='geoposition',
-        routing_key="UAV_global",
-        body=json.dumps(json_data),
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-        ))
+# def global_position_uav_callback(data):
+#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+#     json_data = {"coordinates": {}, "angles": {}}
+#     json_data["coordinates"]["x"] = data.position.x
+#     json_data["coordinates"]["y"] = data.position.y
+#     json_data["coordinates"]["z"] = data.position.z
+#     json_data["angles"]["x"] = data.orientation.x
+#     json_data["angles"]["y"] = data.orientation.y
+#     json_data["angles"]["z"] = data.orientation.z
+#     json_data["angles"]["w"] = data.orientation.w
+#     print(json_data)
+#     channel.basic_publish(
+#         exchange='geoposition',
+#         routing_key="UAV_global",
+#         body=json.dumps(json_data),
+#         properties=pika.BasicProperties(
+#             delivery_mode=2,
+#         ))
+#     time.sleep(10)
 
 
 def voltage_uav_callback(data):
+    time = datetime.datetime.now().time().strftime("%H:%M:%S")
+    global old_voltage_data
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.voltage)
-    json_data = {"charge": data.voltage}
-    channel.basic_publish(
-        exchange='battery',
-        routing_key="UAV_global",
-        body=json.dumps(json_data),
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-        ))
+    voltage = '{:.0f}'.format(data.voltage)
+    json_data = {"battery": voltage}
+    # print(json_data)
+    if old_voltage_data != voltage:
+        print("*******************")
+        print(voltage)
+        print(old_voltage_data)
+        print("************")
+        channel.basic_publish(
+            exchange='battery',
+            routing_key="UAV_global",
+            body=json.dumps(json_data),
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+            ))
+        cursor = connection_db.cursor()
+        insert_query = """ UPDATE uav_dynamic_params SET battery = '{}' WHERE time = '{}' AND id = {};
+                """.format(voltage, time, 1)
+        cursor.execute(insert_query)
+        connection_db.commit()
+        count = cursor.rowcount
+        print(count, "Succesfull update")
+        if count == 0:
+            cursor = connection_db.cursor()
+            insert_query = """ INSERT INTO uav_dynamic_params (time, id, battery)
+                                                       VALUES (%s, %s, %s)"""
+            item_tuple = (time, 1, voltage)
+            cursor.execute(insert_query, item_tuple)
+            connection_db.commit()
+            count = cursor.rowcount
+            print(count, "Succesfull update")
+    old_voltage_data = voltage
+    rospy.sleep(5)
 
 
 def altitude_uav_callback(data):
+    time = datetime.datetime.now().time().strftime("%H:%M:%S")
+    global altitude_old_data
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
-    json_data = {"altitude": data.terrain}
-    print(json_data)
-    channel.basic_publish(
-        exchange='altitude',
-        routing_key="UAV_global",
-        body=json.dumps(json_data),
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-        ))
+    altitude = '{:.1f}'.format(data.terrain)
+    json_data = {"altitude": altitude}
+    if altitude_old_data != altitude:
+        channel.basic_publish(
+            exchange='altitude',
+            routing_key="UAV_global",
+            body=json.dumps(json_data),
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+            ))
+        cursor = connection_db.cursor()
+        insert_query = """ UPDATE uav_dynamic_params SET altitude = '{}' WHERE time = '{}' AND id = {};
+                    """.format(altitude, time, 1)
+        cursor.execute(insert_query)
+        connection_db.commit()
+        count = cursor.rowcount
+        print(count, "Succesfull update altitude")
+        if count == 0:
+            cursor = connection_db.cursor()
+            insert_query = """ INSERT INTO uav_dynamic_params (time, id, altitude)
+                                                           VALUES (%s, %s, %s)"""
+            item_tuple = (time, 1, altitude)
+            cursor.execute(insert_query, item_tuple)
+            connection_db.commit()
+            count = cursor.rowcount
+            print(count, "Succesfull INSERT altitude")
+    altitude_old_data = altitude
+    rospy.sleep(5)
 
 
 def listener():
     rospy.init_node('uav_interface_listener', anonymous=True)
-    rospy.Subscriber("/mavros/local_position", Pose, local_position_uav_callback)
-    rospy.Subscriber("/mavros/global_position", Pose, global_position_uav_callback)
+    rospy.Subscriber("/mavros/local_position/pose", PoseStamped, local_position_uav_callback)
+    rospy.Subscriber("/mavros/global_position", PoseStamped, global_position_uav_callback)
     rospy.Subscriber("/mavros/battery", BatteryState, voltage_uav_callback)
     rospy.Subscriber("/mavros/altitude", Altitude, altitude_uav_callback)
 
