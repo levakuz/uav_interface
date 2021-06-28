cb5c4d38218e5f18fe13db5e782f74dc195ed8d7
levakuz <levakuz007@gmail.com>
2021-06-28 12:34:06 +0300

    fix compose and req

--- src/junk/goal_objects.py
+++ src/junk/goal_objects.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import rospy
-import GoalListenerClass
-
+from src import GoalListenerClass
 
 # def __init__(self, username_rmq, password_rmq, ip_rmq, db_username, db_password, db_ip, db_name, time_interval):
 
 lisener = GoalListenerClass.GoalObjectsListener("admin", "admin", "192.168.0.17", "postgres", "password", "192.168.0.17", "postgres",
-                          20)
+                                                20)
 lisener.listener()
 rospy.spin()
